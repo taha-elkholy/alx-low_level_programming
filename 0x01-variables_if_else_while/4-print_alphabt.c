a8638ad80a88cb30b5eee05cc3cb6921f098952b
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-20 23:20:30 +0000

    print all alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - starter point for the programe
+ *
+ * print all alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		ch = tolower(i);
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
