ae85a78527862d42af94a158ed47044ba3139d3d
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-21 22:51:11 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starter point to the program
+ *
+ * This program prints _putchar
+ * Return 0 on Success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# Functions And nested loops project
