ef3d0d33350116aed8e63b17add51b44afa2380c
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-22 10:59:05 +0000

    C Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - starter point to the program
+ *
+ * this program prints all alphabet in lower case
+ * Return: (0) in Success
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
