b117a63f070d2dd7ea4fea7cad883de7396308e9
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-22 15:24:06 +0000

    C Functions and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints all alphabet
+ *
+ * this program prints all alphabet in lower case 10 times
+ * Return: Always void (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		int k;
+
+		for (k = 97; k <= 122; k++)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
