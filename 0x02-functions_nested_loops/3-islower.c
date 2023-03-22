bc116b27a83565c6830e23903b908a541cb2842c
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-22 16:05:40 +0000

    C Functions and nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - prints all alphabet
+ *@c: Char in ASCII code
+ (*
+ * this program prints all alphabet in lower case 10 times
+ * Return: 1 if lower case char else return 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (ch >= 'A' && ch <= 'Z')
+		return (0);
+	else
+		return (1);
+}
