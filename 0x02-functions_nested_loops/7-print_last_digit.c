9ec8bf8cf4d2778bb79f1f6d853e9a49c7d5c6fb
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-22 19:02:26 +0000

    C Functions and nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return a value of last digit n
+ *@n: number
+ (*
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	return (last);
+	_putchar('\n');
+}
