4ce881f7cfe9428ab3adb49f5ac58616ba6fdaaf
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-22 22:10:49 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * @a: value of number 1
+ * @b: value of number 2
+ *
+ * Return: the addition of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
