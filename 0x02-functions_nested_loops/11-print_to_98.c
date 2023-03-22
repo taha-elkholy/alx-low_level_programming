c8cfda24b8585cadbcb65e303d4f697903397ddc
Taha Elkholy <95647394+taha-elkholy@users.noreply.github.com>
2023-03-23 00:33:45 +0200

    nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,24 +13,31 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		int i;
-
-		for (i = n; i <= 98; i++)
+		while (n <= 98)
 		{
-			printf("%d, ", i);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
 	}
 	else if (n > 98)
 	{
-		int i;
-
-		for (i = n; i >= 98; i--)
+		while (n >= 98)
 		{
-			printf("%d, ", i);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
 		}
 	}
 	else
 	{
 		printf("98");
 	}
+	printf("\n");
 }
