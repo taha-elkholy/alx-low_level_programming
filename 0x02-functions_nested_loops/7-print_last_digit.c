1e36705f116e11cfee12e39be838b6c7306ee091
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-22 19:10:58 +0000

    C Functions and nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,6 +12,15 @@ int print_last_digit(int n)
 	int last;
 
 	last = n % 10;
-	return (last);
+	if (last < 0)
+	{
+		_putchar(-last + 48);
+		return (-last);
+	}
+	else
+	{
+		_putchar(last + 48);
+		return (last);
+	}
 	_putchar('\n');
 }
