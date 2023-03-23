11bfff7058d995102155108fc6667230729c9d4b
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-23 23:11:59 +0000

    More Functions and nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,13 @@
 
 int _isupper(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 	_putchar('\n');
 }
