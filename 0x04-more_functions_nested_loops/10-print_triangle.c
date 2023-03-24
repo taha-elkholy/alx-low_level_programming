82ed7e009236c17813cf87112cba6506a426f6ba
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-24 01:57:32 +0000

    More Functions and nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: triangle size
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int x,y,z;
+
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
