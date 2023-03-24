7a93697e5650ef9b775f5ffcd299af4208cb687a
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-24 01:13:16 +0000

    More Functions and nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - print numbers
+ *
+ * Return: void
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+	printf("\n");
+}
