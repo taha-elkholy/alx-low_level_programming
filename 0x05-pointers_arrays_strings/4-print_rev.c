9436012f3a245afed829e0a2129013064f12863e
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-03-28 00:13:19 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string
+ * @s: str value
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i, length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	return (length);
+
+}
+
+/**
+ * print_rev - print reversed string
+ * @s: string input
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = _strlen(s);
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
