040e854f8165e7c8e16c71d367330eb4b7b0684f
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-03 16:25:16 -0700

    pointers_arrays_strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,16 +7,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; accept[j] != '\0'; j++)
-                {
-                        if (s[i] == accept[j])
-                                return (s + i);
-                }
-        }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
 
-        return (NULL);
-
+	return (0);
+}
