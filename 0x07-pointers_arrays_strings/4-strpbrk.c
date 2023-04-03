5c0b83698b4c83fdbd8635a1e4defa9ca4389816
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-03 16:13:47 -0700

    pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The bytes to be searched against.
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i,j;
+
+	while (*s != '\0')
+	{
+
+	}
+}
