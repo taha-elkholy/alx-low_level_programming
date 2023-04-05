c677678b7372a38bed40d4dc33e9691332ab696a
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-05 16:24:52 -0700

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,39 @@
+int _sqrt_helper(int n, int guess);
+
 /**
- * _sqrt_recursion - a function that get natural sqrt
- * @n: number
+ * _sqrt_recursion - calculates the square root of a number using recursion
+ * @n: the number to find the square root of
  *
- * Return: sqrt of n or -1 if not exist.
+ * Return: the square root of n, or -1 if n does not have a natural square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
+	/* base case: square root of 0 or 1 is itself */
+	if (n == 0 || n == 1)
+		return (n);
+	/* recursive case: improve guess by averaging with n/guess */
+	int guess = _sqrt_helper(n, n / 2);
 
-	return (x * _pow_recursion(x, y - 1));
+	if (guess * guess == n)
+		return (guess);
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_helper - calculates the square root of a number using recursion
+ * @n: the number to find the square root of
+ * @guess: the current guess for the square root
+ *
+ * Return: the improved guess for the square root of n
+ */
+int _sqrt_helper(int n, int guess)
+{
+	/* base case: guess is a good enough approximation */
+	if (guess * guess <= n && (guess + 1) * (guess + 1) > n)
+		return (guess);
+	/*Recursive case: improve guess by averaging with n/guess */
+	int new_guess = (guess + n / guess) / 2;
+
+	return (_sqrt_helper(n, new_guess));
 }
