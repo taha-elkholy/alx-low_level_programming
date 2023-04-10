2034a38dee11e4c793ad1d724c279e66ac62e384
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-10 06:34:46 -0700

    argc argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - starter point to the program
+ *
+ * @argc: number of arguments
+ * @argv: arry of strings related to args names
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins += cents / 25;
+	cents %= 25;
+
+	coins += cents / 10;
+	cents %= 10;
+
+	coins += cents / 5;
+	cents %= 5;
+
+	coins += cents / 2;
+	cents %= 2;
+
+	coins += cents;
+
+	printf("%d\n", coins);
+
+	return (0);
+}
