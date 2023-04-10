bccadc897b3b214e4b3106213f16f57f7bb56ec6
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-10 04:15:56 -0700

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - starter point to the program
+ *
+ * @argc: number of arguments
+ * @argv: arry of strings related to args names
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("my name is %s\n", argv[0]);
+
+	return (0);
+}
