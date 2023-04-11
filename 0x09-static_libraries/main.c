a0f2b7c11b2cb9ef1f9832e464e2e6123689e70d
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-10 18:35:32 -0700

    static lib

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+ /**
+  * main - start point to the program
+  *
+  * Return: 0
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
+}
