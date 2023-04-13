7cd5320a895e3aa467249a7058d5043bd1c83bef
Taha Elkholy <95647394+taha-elkholy@users.noreply.github.com>
2023-04-13 21:10:45 +0200

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: pointer to the allocated memory fails, return 98
+ **/
+ void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+
+    ptr = malloc(b);
+
+    if (ptr == NULL)
+    {
+        exit(98);
+    }
+
+    return (ptr);
+}
