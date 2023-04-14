71efa6175111b5df24517f1c1cbb27408b9bcfae
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-13 17:05:15 -0700

    static

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-void _puts(char *s)
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts(char *str)
 {
-    puts(s);
-}
+	int count = 0;
 
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
