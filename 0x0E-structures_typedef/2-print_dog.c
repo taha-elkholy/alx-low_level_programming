17b800a59abe4ebeaab0116992d3a2ac5317e45f
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-17 06:09:36 -0700

    structures and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,6 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("nothing\n");
-	}
-	else
-	{
 		printf("Name: %s\n", (d.name) ? d.name :  "(nil)");
 		printf("Age: %f\n", (d.age) ? d.age :  0);
 		printf("Owner: %s\n", (d.owner) ? d.owner : "(nil)");
