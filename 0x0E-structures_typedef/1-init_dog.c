28c8f19e8dcdd0bda712f49f74e338280746befc
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-17 05:11:35 -0700

    structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "dog.h"
+
+/**
+ * init_dog - function create new dog with data
+ * @d: the dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
