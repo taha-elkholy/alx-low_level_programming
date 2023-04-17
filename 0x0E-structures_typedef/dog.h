d34068e9da418354fba6705bea12ed01ddf54469
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-04-17 04:56:54 -0700

    structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - descripe a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * A struct that descripe a new type of animals Dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
