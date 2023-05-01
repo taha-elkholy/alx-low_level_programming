607c65f56d087262726fc5caa70737c0636c07eb
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-01 11:49:27 -0700

    sum of n

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,21 @@
+/*
+ * File: 9-insert_nodeint.c
+ * Author: TahaElkholy
+ */
+
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * insert_nodeint_at_index - insert in index
+ * @head: head of linked list
+ * @idx: index
+ * @n: value
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *node;
+}
