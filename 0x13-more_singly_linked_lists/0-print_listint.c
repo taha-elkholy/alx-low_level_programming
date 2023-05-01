07c9b8f53948e794a274ef10b2d90ffc381979d0
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-01 03:40:22 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,14 @@
+/*
+ * File: 0-print_listint.c
+ * Auth: Taha Elkholy
+ */
+
 #include "lists.h"
 #include <stdio.h>
 
 /*
  * print_listint: print all element in a linked list
- * h: head of linked list
+ * @h: head of linked list
  *
  * Return: number of element
  */
