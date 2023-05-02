4e48641d11e2df3503bf0e8cb01afe47746bb72f
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-02 12:48:20 -0700

    delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < (index - 1); i++)
 	{
-		if (temp == NULL || temp->next == NULL)
+		if (temp->next == NULL)
 			return (-1);
 
 		temp = temp->next;
