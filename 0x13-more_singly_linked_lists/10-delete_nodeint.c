66dea36a97f62377b2814c8e3e9e6db4ee7c5504
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-02 13:27:48 -0700

    delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,10 +18,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *node, *temp = *head;
 
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
+	if (*head == NULL)
 		return (-1);
-
 	if (index == 0)
 	{
 		*head = (*head)->next;
