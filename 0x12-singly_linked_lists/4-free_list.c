4b9f446b559b0703ab68580b2fbd9cad38dbb3d6
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-03 05:19:17 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -15,12 +15,12 @@
 void free_list(list_t *head)
 {
 	list_t *node;
-	
+
 	while (head)
 	{
-	       node =  head->next;
-	       free(head->str);
-	       free(head);
-	       head = node;
+		node = head->next;
+		free(head->str);
+		free(head);
+		head = node;
 	}
 }
