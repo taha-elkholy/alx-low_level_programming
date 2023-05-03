e7d17876ce52c8bcd2139a02ec7bc814a195b05a
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-03 03:23:28 -0700

    number of elements

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+/*
+ * File: 2-add_node.c
+ * Author: Created By TahaElkholy
+ */
+
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - add new element in the bigin
+ * @head: head
+ * @str: string to be added
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int n;
+	list_t *node;
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (Null);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	for (n = 0; str[n];)
+		n++;
+	node->str = dup;
+	node->n = n;
+	node->next = *head;
+	*head = node;
+	return (node);
+}
