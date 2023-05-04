54688b91302c38d42617271f7ccbe275025b30f5
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-04 09:30:33 -0700

    Bit Manipulation in c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+/*
+ * FileName: 0-binary_to_uint.c
+ * Author: This file created by TahaElkholy
+ */
+
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - convert binary to unit
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+}
