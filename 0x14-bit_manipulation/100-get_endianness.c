a393c8b15eae228cb21256e035e3f937ff31a5c5
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-05 09:07:23 -0700

    Bit Manipulation in c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+/*
+ * FileName: 100-get_endianness.c
+ * Author: This file created by TahaElkholy
+ */
+
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness.
+ *
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	int num = 1;
+	
+	return (*((char *) &num) + '0');
+}
