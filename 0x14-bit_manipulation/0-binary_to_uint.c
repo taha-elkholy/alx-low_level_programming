fdf9ef89cbfcbb47a123e133b6cd2cd404252201
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-05 06:38:14 -0700

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,6 +5,8 @@
 
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * binary_to_uint - convert binary to unit
@@ -14,4 +16,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int res = 0;
+	int i, len = strlen(b);
+
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] == '1')
+			res |= 1 << (len - 1 - i);
+		else if (b[i] != '0')
+			return (0);
+	}
+	return (res);
 }
