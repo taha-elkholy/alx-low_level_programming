7f5058b6fb618f289e12bfefad5070a7f9f77d04
taha-elkholy <tahaelkholy.dev@gmail.com>
2023-05-10 04:30:49 -0700

    File I/O Project Tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 	if (file2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(file1), exit(99);
+		exit(99);
 	}
 	while (n1 == 1024)
 	{
